edc457d713dd81c5a7a08a5b6e3a0c27cb932e7c
Tyler <tprincipe@brooksschool.org>
2025-03-31 14:12:25 -0400

    yolo

--- real-javascript/myScript.js
+++ real-javascript/myScript.js
@@ -3,3 +3,17 @@ document.getElementById('mine').innerHTML = 'no'
 window.alert('i am an alert')
 
 console.log('hello world')
+
+function print(){
+    let msg = document.getElementById("name").value
+    console.log(msg)
+
+    let txt = document.getElementById('animal').value
+    console.log(txt)
+    message.innerHTML = "hey " + msg + " dont you wish you were a " + txt
+}
+
+function colorSwitch(){
+    document.getElementById('name').style.backgroundColor='red'
+
+}
